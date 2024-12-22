e93d58975d83893401bc938ab57d36e754e924ac
Asier <asierbanuelosdelafuente@gmail.com>
2024-12-22 23:00:45 +0100

    PopUp

--- src/app/shared/barra-control/barra-control.component.ts
+++ src/app/shared/barra-control/barra-control.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { PopcitaComponent } from '../popcita/popcita.component';
+
 
 @Component({
   selector: 'app-barra-control',
@@ -8,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class BarraControlComponent implements OnInit {
   fechaActual: string='';
 
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {
     // fecha actual
@@ -24,5 +27,14 @@ export class BarraControlComponent implements OnInit {
     const fecha = new Date(this.fechaActual);
     fecha.setDate(fecha.getDate() + 1);
     this.fechaActual = fecha.toISOString().split('T')[0];
+  }
+
+  async abrirPopup() { //Metodo para abrir el componente de citas
+    const modal = await this.modalCtrl.create({
+      component: PopcitaComponent,
+      cssClass: 'custom-modal', 
+
+    });
+    await modal.present();
   }
 }
