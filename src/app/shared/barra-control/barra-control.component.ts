142313c41afaeaf96355c63ebf049a5bdc1347f9
Asier <asierbanuelosdelafuente@gmail.com>
2024-12-22 22:30:12 +0100

    Mas mejoras (Optimizacion)

--- src/app/shared/barra-control/barra-control.component.ts
+++ src/app/shared/barra-control/barra-control.component.ts
@@ -5,10 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './barra-control.component.html',
   styleUrls: ['./barra-control.component.scss'],
 })
-export class BarraControlComponent  implements OnInit {
+export class BarraControlComponent implements OnInit {
+  fechaActual: string='';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // fecha actual
+    this.fechaActual = new Date().toISOString().split('T')[0];
+  }
+  retrocederFecha() {
+    const fecha = new Date(this.fechaActual);
+    fecha.setDate(fecha.getDate() - 1);
+    this.fechaActual = fecha.toISOString().split('T')[0];
+  }
 
+  avanzarFecha() {
+    const fecha = new Date(this.fechaActual);
+    fecha.setDate(fecha.getDate() + 1);
+    this.fechaActual = fecha.toISOString().split('T')[0];
+  }
 }
